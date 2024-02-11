55de14f09b673bb505af11639ca853a8b81b301b
Vyacheslav Chernov <vchernov@gmail.com>
2024-02-12 00:08:58 +0300

    added ship common interface

---
+++ interfaces/ship.common.interface.ts
@@ -0,0 +1,5 @@
+import ShipType from '../types/ship.type.ts';
+
+export interface ShipCommon {
+    type: ShipType;
+}
--- types/ship.type.ts
+++ types/ship.type.ts
@@ -1,1 +1,16 @@
-type ShipType = 'TS' | 'TL' | 'TM' | 'M1' | 'M2' | 'M3' | 'M4' | 'M5' | 'M6' | 'M7' | 'M8';
+export enum ShipType {
+    TS = 'TS',
+    TL = 'TL',
+    TM = 'TM',
+    M1 = 'M1',
+    M2 = 'M2',
+    M3 = 'M3',
+    M4 = 'M4',
+    M5 = 'M5',
+    M6 = 'M6',
+    M7 = 'M7',
+    M8 = 'M8',
+}
+
+export type ShipTypeKey = keyof typeof ShipType;
+export type ShipTypeValue = typeof ShipType[ShipTypeKey];
